da25272e676ac074a32bc5ba450d9d1063765503
Keang <skeang@gmail.com>
2014-12-21 13:37:48 +0800

    Add book fetching, and refactor

--- js/controllers.js
+++ js/controllers.js
@@ -6,18 +6,23 @@ prefalyticsApp.config(function(FacebookProvider){
 
 prefalyticsApp.controller('HomeCtrl', function($scope, Facebook){
   $scope.user = {first_name: "there."};
+  $scope.users = {};
+  $scope.books = {};
 
-  var setStatus = function(){
-    Facebook.getLoginStatus(function(response){
-      if(response.status === 'connected'){
-        $scope.loggedIn = true;
-        setUser();
-      } else {
-        $scope.loggedIn = false;
-      }
-    });
-  }
-  setStatus();
+  
+  var setStatus = function(response){
+    if(response.status === 'connected'){
+      $scope.loggedIn = true;
+      setUser();
+    } else {
+      $scope.loggedIn = false;
+    }
+  };
+
+  Facebook.getLoginStatus(function(response){
+    setStatus(response)
+    console.log($scope.loggedIn)
+  });
 
   var setUser = function(){
     Facebook.api('/me', function(response){
@@ -28,10 +33,9 @@ prefalyticsApp.controller('HomeCtrl', function($scope, Facebook){
   var login = function(){
     if($scope.loggedIn) return;
     Facebook.login(function(response){
-      console.log("logged in")
-      console.log(response);
-      setStatus();
-    });
+      console.log(response.authResponse.grantedScopes);
+      setStatus(response);
+    }, {scope:"user_likes,user_actions.books,user_friends", return_scopes:true});
   }
 
   $scope.Logout = function(){
@@ -44,5 +48,38 @@ prefalyticsApp.controller('HomeCtrl', function($scope, Facebook){
     if(!$scope.loggedIn){
       login();
     }
+  }
+
+  var countBookLikes = function(graph_response){
+    graph_response.data.forEach(function(book){
+      title = book.name
+      if(typeof $scope.books[title] === "undefined")
+        $scope.books[title] = 1;
+      else $scope.books[title] += 1;
+    });
+  }
+
+  var countBookReads = function(graph_response){
+    graph_response.data.forEach(function(action){
+      title = action.data.book.title
+      if(typeof $scope.books[title] === "undefined")
+        $scope.books[title] = 1;
+      else $scope.books[title] += 1;
+    });
+  }
+
+  $scope.GetBooks = function(user){
+    if(!$scope.loggedIn){
+      console.log("Not logged in");
+      return;
+    }
+    
+    u = typeof user === 'undefined' ? "me" : user;
+    Facebook.api("/" + u + "/books", function(response){
+      countBookLikes(response)
+    })
+    Facebook.api("/" + u + "/books.reads", function(response){
+      countBookReads(response)
+    })
   }
 });
